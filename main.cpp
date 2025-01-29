2e8535126fb430c394455a15ba45c43685b1b02a
mWizzard <merlinsuter@outlook.com>
2025-01-29 09:38:23 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * \file main.cpp
+ * \author mWizzard
+ * 
+ * \brief Main file where the data structures are compared and used
+ * 
+ * \date 2025-01-29
+ */
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, World\n";
+
+    return 0;
+}
